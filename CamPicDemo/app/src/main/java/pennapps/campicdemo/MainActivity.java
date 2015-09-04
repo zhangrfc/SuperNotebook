b2763cba451f9d9d4386a7e6d8d08a560769bb45
BowenBao <semisqg@gmail.com>
2015-09-04 19:42:00 -0400

    New Act, Cam with Recog

--- CamPicDemo/app/src/main/java/pennapps/campicdemo/MainActivity.java
+++ CamPicDemo/app/src/main/java/pennapps/campicdemo/MainActivity.java
@@ -30,6 +30,7 @@ import java.util.Date;
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_TAKE_PHOTO = 1;
+    private static final int REQUEST_TAKE_PHOTO_PAINT = 2;
     private String mCurrentPhotoPath;
     private String WORD_PIC_PATH;
     //private static final String WORD_PIC_PATH = "/storage/15FC-271E/Android/data/pennapps.campicdemo/files/word.jpg";
@@ -114,18 +115,31 @@ public class MainActivity extends AppCompatActivity {
         this.sendBroadcast(mediaScanIntent);
     }
 
+    protected boolean checkIfImgExists(String path) {
+        // Check if image really exist
+        File image = new File(mCurrentPhotoPath);
+        if (image.exists()) {
+            Log.i("image", "file exist.");
+            Log.i("image", mCurrentPhotoPath);
+            return true;
+        } else Log.i("image", "file does not exist. ");
+        return false;
+        // Pass the path of photo to next activity.
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Old request
         if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
             super.onActivityResult(requestCode, resultCode, data);
             // Check if image really exist
-            File image = new File(mCurrentPhotoPath);
-            if (image.exists()) {
-                Log.i("image", "file exist.");
-                Log.i("image", mCurrentPhotoPath);
-            } else Log.i("image", "file does not exist. ");
-            // Pass the path of photo to next activity.
-            openPicWordActivity(mCurrentPhotoPath);
+            if (checkIfImgExists(mCurrentPhotoPath))
+                openPicWordActivity(mCurrentPhotoPath);
+        } else if (requestCode == REQUEST_TAKE_PHOTO_PAINT && resultCode == RESULT_OK) {
+            super.onActivityResult(requestCode, resultCode, data);
+            // Check if image really exist
+            if (checkIfImgExists(mCurrentPhotoPath))
+                openCamPaintActivity(mCurrentPhotoPath);
         }
     }
     // Test function, enter text selection mode.
@@ -144,6 +158,12 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void openCamPaintActivity(String imgPath) {
+        Intent intent = new Intent(getApplicationContext(), CamPaintActivity.class);
+        intent.putExtra("IMAGE", imgPath);
+        startActivity(intent);
+    }
+
     public void wordPick(View view) {
         Intent intent = new Intent(getApplicationContext(), PickWordActivity.class);
         intent.putExtra("IMAGE", WORD_PIC_PATH);
@@ -154,6 +174,20 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(getApplicationContext(), PaintActivity.class);
         intent.putExtra("IMAGE", WORD_PIC_PATH);
         startActivity(intent);
+    }
+
+    public void camPaintDemo(View view) {
+        // No need for camera permission. wired
+        File image = null;
+        try {
+            image = createImageFile();
+        } catch (IOException ex) {
+            Log.e("exception", ex.toString());
+        }
+        mCurrentPhotoPath = image.getAbsolutePath();
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(image));
+        startActivityForResult(intent, REQUEST_TAKE_PHOTO_PAINT);
     }
 
     // Test Tesseract
