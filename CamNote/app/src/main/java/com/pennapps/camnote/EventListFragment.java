d29e991f784839f2beb0802f9c2a407977f5005c
Qingxiao Dong <qingxiao.dong@gmail.com>
2015-09-05 21:55:00 -0400

    Nit.

--- CamNote/app/src/main/java/com/pennapps/camnote/EventListFragment.java
+++ CamNote/app/src/main/java/com/pennapps/camnote/EventListFragment.java
@@ -131,8 +131,8 @@ public class EventListFragment extends Fragment{
         Log.d(Integer.toString(inDB.numberOfRows()), "numofRows");
 =======
         CurrentID = inDB.numberOfRows();
-        inDB.insertNote("Title", "context", "time", "date", "host", "add", "pic", "cat");
-        inDB.insertNote("Title2", "context", "time", "date", "host", "add", "pic", "cat");
+        //inDB.insertNote("Title", "context", "time", "date", "host", "add", "pic", "cat");
+        //inDB.insertNote("Title2", "context", "time", "date", "host", "add", "pic", "cat");
         Log.d(Integer.toString(CurrentID), "currentID");
 >>>>>>> origin/master
     }
